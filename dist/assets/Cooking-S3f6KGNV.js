01d3e6796753adb2c99c5d8063825d9c021a861b
Klevis Imeri <klevisimeri11@gmail.com>
2025-09-13 11:17:34 +0200

    deploy

---
+++ dist/assets/Cooking-S3f6KGNV.js
@@ -0,0 +1,22 @@
+import{_ as n}from"./MarkdownRenderer.vue_vue_type_script_setup_true_lang-YxJnBujK.js";import{d as e,c as a,u as t,o}from"./index-CiHukJAr.js";const i=`<div style="position: relative; margin: 0 auto; text-align: center;">
+  <div style="display: inline-block; text-align: center;">
+    <div style="font-size: 1.5rem; font-weight: bold; margin-bottom: 0.5em;">
+            Cooking Delicious Food Is One Of My Hobbies
+    </div>
+  </div>
+</div>
+
+---
+
+#### These are the foods I can cook:
+- Chicken with many spices, bread and black tea
+- Rukola Salat with Parmixan cheese, Orange and Olive and Tomatoes
+- Pasta Carbonara
+- Eeggs with milk, salam, back tea with lemon and white cheese
+- Pig meat with garlic and peper. 
+
+#### Snakcs I need to buy:
+- Doughuts
+- Banana
+- Sushi 
+`,m=e({__name:"Cooking",setup(s){return(c,r)=>(o(),a(n,{content:t(i)},null,8,["content"]))}});export{m as default};
