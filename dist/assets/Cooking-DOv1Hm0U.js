63d3f687029c32b06004d23989b65b9b70b71db9
Klevis Imeri <klevisimeri11@gmail.com>
2025-09-13 11:33:17 +0200

    deploy

--- dist/assets/Cooking-DOv1Hm0U.js
+++ dist/assets/Cooking-DOv1Hm0U.js
@@ -1,4 +1,4 @@
-import{_ as n}from"./MarkdownRenderer.vue_vue_type_script_setup_true_lang-YxJnBujK.js";import{d as e,c as a,u as t,o}from"./index-CiHukJAr.js";const i=`<div style="position: relative; margin: 0 auto; text-align: center;">
+import{_ as n}from"./MarkdownRenderer.vue_vue_type_script_setup_true_lang-DmsRPKt1.js";import{d as e,c as a,u as t,o}from"./index-BmeL3flB.js";const i=`<div style="position: relative; margin: 0 auto; text-align: center;">
   <div style="display: inline-block; text-align: center;">
     <div style="font-size: 1.5rem; font-weight: bold; margin-bottom: 0.5em;">
             Cooking Delicious Food Is One Of My Hobbies
@@ -14,6 +14,7 @@ import{_ as n}from"./MarkdownRenderer.vue_vue_type_script_setup_true_lang-YxJnBu
 - Pasta Carbonara
 - Eeggs with milk, salam, back tea with lemon and white cheese
 - Pig meat with garlic and peper. 
+- Patatoes with white cheese and oragano + some type of meat (above)
 
 #### Snakcs I need to buy:
 - Doughuts
